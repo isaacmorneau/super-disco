60b24aa1bb7c8dfc98973b5c90e3852945ee9d77
Isaac <isaacmorneau@gmail.com>
2017-07-26 23:16:57 -0700

    now its all mine; works fine just need to replace gethostbyname

--- src/http_client.c
+++ src/http_client.c
@@ -10,345 +10,102 @@
 #include <unistd.h>
 #include <netdb.h>
 
-#define PORT 80
-#define MAX_RETRIES 5
 
-#define TRUE 1
-#define FALSE 0
+int read_all(int fd, void * buffer, int size) {
+    int bytes_read = 0, total_read = 0;
+    while ((bytes_read = read(fd, buffer+total_read, size-total_read)) > 0) {
+        total_read += bytes_read;
+    }
+    return total_read ? total_read : -1;
+}
 
-int sockfd;
-struct sockaddr_in address;
+int write_all(int fd, void * buffer, int size) {
+    int bytes_wrote = 0, total_wrote = 0;
+    while ((bytes_wrote = write(fd, buffer+total_wrote, size-total_wrote)) > 0) {
+        total_wrote += bytes_wrote;
+    }
+    return total_wrote ? total_wrote : -1;
+}
 
-int content_length = -1;
-char location[1024];
-int header_err_flag = FALSE;
+int send_get(int fd, char * location, char * server, int port) {
+    char buffer[4096];
+    sprintf(buffer, 
+            "GET %s HTTP/1.1\r\n"
+            "Host: %s:%d\r\n"
+            "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36\r\n"
+            "Connection: Close\r\n"
+            "\r\n",
+            location, server, port);
+    return write_all(fd, buffer, strlen(buffer));
+}
 
-int read_line(int fd, char *buffer, int size) {
-    char next = '\0';
-    char err;
-    int i = 0;
-    while ((i < (size - 1)) && (next != '\n')) {
-        err = read(fd, &next, 1);
-
-        if (err <= 0) 
+int read_response(int fd) {
+    int allocated = 2046;
+    int current_pos = 0;
+    void * buffer = malloc(allocated);
+    int resp_read = 0;
+    while ((resp_read = read_all(fd, buffer+current_pos, allocated-current_pos))) {
+        if (resp_read == -1) {
+            //something broke /shrug
+            free(buffer);
+            return -1;
+        } else if (resp_read < allocated) {
+            //we hit the end
             break;
-
-        if (next == '\r') {
-            err = recv(fd, &next, 1, MSG_PEEK);
-            if (err > 0 && next == '\n') {
-                read(fd, &next, 1);
-            } else {
-                next = '\n';
+        } else if (resp_read == allocated) {
+            //we need more room
+            allocated *= 2;
+            void * temp;
+            if (!(temp = realloc(buffer, allocated))) {
+                //probably ran out of memory
+                free(buffer);
+                return -1;
             }
+            buffer = temp;
+            current_pos += resp_read;
         }
-        buffer[i] = next;
-        i++;
     }
-    buffer[i] = '\0';
-    return i;
+    printf("%s", buffer);
+    free(buffer);
+    return 0;
 }
 
-int read_socket(int fd, char *buffer, int size) {
-    int bytes_recvd = 0;
-    int retries = 0;
-    int total_recvd = 0;
-
-    while (retries < MAX_RETRIES && size > 0 && strstr(buffer, ">") == NULL) {
-        bytes_recvd = read(fd, buffer, size);
-
-        if (bytes_recvd > 0) {
-            buffer += bytes_recvd;
-            size -= bytes_recvd;
-            total_recvd += bytes_recvd;
-        } else {
-            retries++;
-        }
-    }
-
-    if (bytes_recvd >= 0) {
-        // Last read was not an error, return how many bytes were recvd
-        return total_recvd;
-    }
-    // Last read was an error, return error code
-    return -1;
-}
-
-int write_socket(int fd, char *msg, int size) {
-    int bytes_sent = 0;
-    int retries = 0;
-    int total_sent = 0;
-
-    while (retries < MAX_RETRIES && size > 0) {
-        bytes_sent = write(fd, msg, size);
-
-        if (bytes_sent > 0) {
-            msg += bytes_sent;
-            size -= bytes_sent;
-            total_sent += bytes_sent;
-        } else {
-            retries++;
-        }
-    }
-
-    if (bytes_sent >= 0) {
-        // Last write was not an error, return how many bytes were sent
-        return total_sent;
-    }
-    // Last write was an error, return error code
-    return -1;
-}
-
-void read_headers() {
-    printf("==>headers<==\n");
-    while(1) {
-        char header[8096];
-        int len;
-        char next;
-        int err;
-        char *header_value_start;
-        len = read_line(sockfd, header, sizeof(header));
-
-        if (len <= 0) {
-            // Error in reading from socket
-            header_err_flag = TRUE;
-            continue;
-        }
-
-        printf("%s", header);
-
-        if (strcmp(header, "\n") == 0) {
-            // Empty line signals end of HTTP Headers
-            return;
-        }
-
-        // If the next line begins with a space or tab, it is a continuation of the previous line.
-        err = recv(sockfd, &next, 1, MSG_PEEK);
-        while (isspace(next) && next != '\n' && next != '\r') {
-            if (err) {
-                printf("header space/tab continuation check err\n");
-                // Not sure what to do in this scenario
-            }
-            // Read the space/tab and get rid of it 
-            read(sockfd, &next, 1);
-
-            // Concatenate the next line to the current running header line
-            len = len + read_line(sockfd, header + len, sizeof(header) - len);
-            err = recv(sockfd, &next, 1, MSG_PEEK);
-        }
-
-        // Find first occurence of colon, to split by header type and value
-        header_value_start = strchr(header, ':');
-        if (header_value_start == NULL) {
-            // Invalid header, not sure what to do in this scenario
-            printf("invalid header\n");
-            header_err_flag = TRUE;
-            continue;
-        }
-        int header_type_len = header_value_start - header;
-
-        // Increment header value start past colon
-        header_value_start++;
-        // Increment header value start to first non-space character
-        while (isspace(*header_value_start) && (*header_value_start != '\n') && (*header_value_start != '\r')) {
-            header_value_start++;
-        }
-        int header_value_len = len - (header_value_start - header);
-
-        // We only care about Content-Length
-        if (strncasecmp(header, "Content-Length", header_type_len) == 0) {
-            content_length = atoi(header_value_start);
-        } else if (strncasecmp(header, "Location", header_type_len) == 0) {
-            strncpy(location, header_value_start, header_value_len);
-        }
-    }
-}
-
-void print_usage() {
-    printf("Usage: ./httpget http://address/file.extension\n");
-}
-
-char* split_path(char *path, char **file_out) {
-    char *pos = strchr(path, '/');
-    if (pos == NULL) {
-        char *addr = (char*) malloc(strlen(path) + 1);
-        char *file = (char*) malloc(2);
-        strncpy(addr, path, strlen(path));
-        strncpy(file, "/", 1);
-        *file_out = file;
-        return addr;
-    }
-    int len_addr = pos - path;
-    int len_file = strlen(path) - (pos - path);
-
-    char *addr = (char*) malloc(len_addr + 1);
-    char *file = (char*) malloc(len_file + 1);
-
-    strncpy(addr, path, len_addr);
-    addr[len_addr] = '\0';
-
-    strcpy(file, pos);
-
-    *file_out = file;
-    return addr;
-}
-
-int split_port(char *addr) {
-    char *pos = strrchr(addr, ':');
-    if (pos == NULL) {
-        return PORT;
-    }
-    int port = atoi(pos + 1);
-    *pos = '\0';
-    return port;
-}
-
-int main(int argc, char *argv[]) {
-    int arg_count = argc - 1;
-    if (arg_count < 1 || arg_count > 1) {
-        print_usage();
+int main(int argc, char ** argv) {
+    int sock;
+    int port = 80;
+    struct sockaddr_in addr;
+    
+    if (argc < 2) {
+        printf("Please Specify Url\n format: www.example.com or www.example.com /location/requested");
         return 1;
     }
 
-    strcpy(location, "\0");
-
-    // The host address and file we wish to GET, e.g. http://127.0.0.1/blah.txt
-    char *addr_start = argv[1];
-
-    // Skip past http:// if it's there
-    if (strncasecmp(addr_start, "http://", strlen("http://")) == 0) {
-        addr_start += strlen("http://");
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        return 2;
     }
 
-    // Split the host address into a web address and a file name/path, 
-    // e.g. 127.0.0.1/blah.txt into 127.0.0.1 and /blah.txt
-    char *file_name;
-    char *server_address = split_path(addr_start, &file_name);
-    int port = split_port(server_address);
-    struct hostent *host_entity = gethostbyname(server_address);
+    //TODO this has been depricated a while but i dont feel like implementing getaddrinfo yet
+    struct hostent *host_entity = gethostbyname(argv[1]);
+    addr.sin_family = AF_INET;
+    memcpy(&addr.sin_addr, host_entity->h_addr_list[0], host_entity->h_length);
 
-    printf("Get file %s at server %s on port %d\n", file_name, server_address, port);
+    addr.sin_port = htons(port);
 
-    // Open up a socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        printf("Unable to open socket\n"); 
-        free(file_name);
-        free(server_address);
-        exit(1);
-        return 1;
+    if (connect(sock, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        close(sock);
+        return 3;
     }
 
-    // Try to connect to server_address on port PORT
-    address.sin_family = AF_INET;
-    memcpy(&address.sin_addr, host_entity->h_addr_list[0], host_entity->h_length);
-    // address.sin_addr.s_addr = inet_addr(server_address);
-    address.sin_port = htons(port);
-
-    if (connect(sockfd, (struct sockaddr*) &address, sizeof(address)) < 0) {
-        printf("Unable to connect to host\n");
-        free(file_name);
-        free(server_address);
-        exit(1);
-        return 1;
-    }
-
-    printf("Connected with host\n");
-    char buffer[8096];
-    sprintf(buffer, "GET %s HTTP/1.1\r\n", file_name);
-    write_socket(sockfd, buffer, strlen(buffer));
-    sprintf(buffer, "Host: %s:%d\r\n", server_address, port);
-    write_socket(sockfd, buffer, strlen(buffer));
-    sprintf(buffer, "User-Agent: Test HTTP Client\r\n");
-    write_socket(sockfd, buffer, strlen(buffer));
-    sprintf(buffer, "Connection: close\r\n");
-    write_socket(sockfd, buffer, strlen(buffer));
-
-    // Signal end of headers with empty line
-    sprintf(buffer, "\r\n");
-    write_socket(sockfd, buffer, strlen(buffer));
-    // Read first line and print to console
-    int len = read_line(sockfd, buffer, sizeof(buffer));
-
-    if (len <= 0) {
-        printf("No response received from server\n");
-        goto error;
+    if (argc == 2 ) {
+        send_get(sock, "/", argv[1], port);
     } else {
-        char time_buffer[512];
-        time_t raw_time;
-        struct tm *current_time;
-        time(&raw_time);
-        current_time = localtime(&raw_time);
-        strftime(time_buffer, 512, "%a, %d %b %Y %T %Z", current_time);
-        printf("[%s] Response received from server\n", time_buffer);
+        send_get(sock, argv[2], argv[1], port);
     }
 
-    // printf("%s\n", buffer);
-    char version[16];
-    char response_code[16];
-    char response_reason[256];
-    unsigned int i, j;
+    read_response(sock);
 
-    // Read version
-    for(i = 0; i < sizeof(version) - 1 && !isspace(buffer[i]); i++) {
-        version[i] = buffer[i];
-    }
-    version[i] = '\0';
-
-    // Skip over spaces
-    for (; isspace(buffer[i]) && i < sizeof(buffer); i++);
-
-    // Read response code
-    for (j = 0; i < sizeof(buffer) && j < sizeof(response_code) - 1 && !isspace(buffer[i]); i++, j++) {
-        response_code[j] = buffer[i];
-    }
-    response_code[j] = '\0';
-
-    // Skip over spaces
-    for (; isspace(buffer[i]) && i < sizeof(buffer); i++);
-
-    // Read response reason
-    for (j = 0; i < sizeof(buffer) && j < sizeof(response_reason) - 1 && buffer[i] != '\n'; i++, j++) {
-        response_reason[j] = buffer[i];
-    }
-    response_reason[j] = '\0';
-
-    // printf("Version: %s\n", version);
-    // printf("Response Code: %s\n", response_code);
-    // printf("Response Reason: %s\n", response_reason);
-
-    read_headers();
-
-    if (strcmp(response_code, "200") != 0) {
-        if (strcmp(response_code, "301") == 0) {
-            printf("%s Error: %s to %s\n", response_code, response_reason, location);
-        } else {
-            printf("%s Error: %s\n", response_code, response_reason);
-        }
-        goto error;
-    }
-
-    if (header_err_flag) {
-        printf("Error reading headers\n");
-        goto error;
-    }
-
-    if (content_length <= 0) {
-        printf("No content received from server\n"); 
-        goto error;
-    }
-
-    char *result = (char*) malloc(content_length + 1);
-    len = read_socket(sockfd, result, content_length);
-    if (len <= 0)
-        goto error;
-    result[len] = '\0';
-
-    printf("%s\n", result); 
-    free(result);
-error:
-    free(file_name);
-    free(server_address);
-    close(sockfd);
-
+    //shutdown happens within the close so dont bother with shutdown
+    close(sock);
     return 0;
 }
